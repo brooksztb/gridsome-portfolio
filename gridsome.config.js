c64d7f5a4a49081441b8cdd4e3862b177d50dcdb
Zachary.Brooks@flyfrontier.com <Zachary.Brooks@flyfrontier.com>
2019-03-27 16:32:03 -0600

    Added netlify cms configuration and base for projects collections

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Zachary Brooks: Full Stack Developer',
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss'
---
+++ src/pages/Projects.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    metaInfo: {
+        title: 'Projects'
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
