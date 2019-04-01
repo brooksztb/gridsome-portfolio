f2188803ccdb21901749879fba1178fe5d472eb6
Zach B <31398142+brooksztb@users.noreply.github.com>
2019-04-01 09:51:31 -0600

    Moved netlify cms script to index.vue and removed vue script plugin

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,6 +8,11 @@
 export default {
   metaInfo: {
     title: 'Hello, world!'
+  },
+  mounted() {
+    let netlifyScript = document.createElement('script');
+    netlifyScript.setAttribute('src', 'https://identity.netlify.com/v1/netlify-identity-widget.js');
+    document.head.appendChild(netlifyScript);
   }
 }
 </script>
