59a64f2c2fb55bc888ce929637277b9d1aa72660
Zach B <31398142+brooksztb@users.noreply.github.com>
2019-04-02 16:58:10 -0600

    Added fix for vuetify plugin with gridsome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue';
 import Vuetify from 'vuetify';
+import DefaultLayout from '~/layouts/Default.vue';
 
 import 'vuetify/dist/vuetify.min.css';
 
