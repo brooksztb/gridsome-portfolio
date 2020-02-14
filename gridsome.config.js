90af032fa470b83d651bbbbe78b0875fd8f4fafd
Brooksztb <brooksztb@gmail.com>
2020-02-14 10:22:45 -0700

    Updated forestry config for instant preview
    Updated forestry config for instant preview

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,43 +7,44 @@ const tailwindcss = require('tailwindcss');
 const config = require('./src/content/data/config.json');
 
 module.exports = {
-	siteName: config.title,
-	metadata: {
-		siteName: config.title,
-		siteDescription: config.description,
-	},
-	siteUrl: 'https://zacharybrooks.io',
-	icon: 'src/static/favicon.ico',
-	transformers: {
-		remark: {
-			externalLinksTarget: '_blank',
-			externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-		},
-	},
-	css: {
-		loaderOptions: {
-			postcss: {
-				plugins: [tailwindcss],
-			},
-		},
-	},
-	plugins: [
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				typeName: 'Blog',
-				baseDir: './src/content/',
-				path: 'blog/**/*.md',
-				resolveAbsolutePaths: true,
-			},
-		},
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				typeName: 'Content',
-				baseDir: './src/content/pages/',
-				path: '*.md',
-			},
-		},
-	],
+  siteName: config.title,
+  metadata: {
+    siteName: config.title,
+    siteDescription: config.description
+  },
+  siteUrl: 'https://zacharybrooks.io',
+  titleTemplate: `%s | ${config.title}`,
+  icon: 'src/static/favicon.ico',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer']
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [tailwindcss]
+      }
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Blog',
+        baseDir: './src/content/',
+        path: 'blog/**/*.md',
+        resolveAbsolutePaths: true
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Content',
+        baseDir: './src/content/pages/',
+        path: '*.md'
+      }
+    }
+  ]
 };
