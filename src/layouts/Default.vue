9f67ca189193542e5d8e2f1f27dbd243b758b19f
Zach Brooks <hcaz1444@gmail.com>
2020-03-07 19:47:21 -0700

    Added Mobile Header
    Added Mobile Header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,95 +1,178 @@
 <template>
-<div id="portfolio" class="font-body">
-  <header
-    class="fixed top-0 flex flex-col items-center justify-between h-screen nav-bar w-10 md:w-15 lg:w-px300 xl:w-px320"
-  >
-    <div
-      class="flex flex-col items-center justify-start mx-auto w-full pt-0 md:pt-9 px-0 lg:px-8 md:h-full"
-    >
-      <g-link to="/">
-        <g-image
-          class="flex md:inline-flex w-full h-6 sm:h-8 md:h-16 lg:h-logo p-2 lg:pb-5"
-          src="/uploads/logo.svg"
-          alt="zachary brooks logo"
-        />
-      </g-link>
+	<div id="portfolio" class="font-body">
+		<header
+			class="fixed w-full z-10 sm:flex md:hidden items-center justify-between flex-wrap nav-bar p-3"
+		>
+			<div class="flex items-center justify-between flex-shrink-0 text-white">
+				<g-link to="/">
+					<g-image
+						class="inline-flex w-full h-10"
+						src="/uploads/logo.svg"
+						alt="zachary brooks logo"
+					/>
+				</g-link>
+				<div class="block">
+					<button
+						@click="mobileNavOpen = !mobileNavOpen"
+						class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"
+					>
+						<svg
+							class="fill-current h-3 w-3"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<title>Menu</title>
+							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+						</svg>
+					</button>
+				</div>
+			</div>
 
-      <g-link
-        class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
-        to="/about"
-        active-class="border border-white"
-      >
-        <font-awesome class="lg:mr-2" :icon="['fas', 'user']" size="lg" />
-        <span class="hidden lg:block">About</span>
-      </g-link>
-      <g-link
-        class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
-        to="/blog"
-        active-class="border border-white"
-      >
-        <font-awesome class="lg:mr-2" :icon="['fas', 'newspaper']" size="lg" />
-        <span class="hidden lg:block">Blog</span>
-      </g-link>
-      <g-link
-        class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
-        to="/skills"
-        active-class="border border-white"
-      >
-        <font-awesome class="lg:mr-2" :icon="['fas', 'code']" size="lg" />
-        <span class="hidden lg:block">Skillset</span>
-      </g-link>
-      <g-link
-        class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
-        to="/work"
-        active-class="border border-white"
-      >
-        <font-awesome class="lg:mr-2" :icon="['fas', 'project-diagram']" size="lg" />
-        <span class="hidden lg:block">Work</span>
-      </g-link>
-    </div>
-    <div
-      class="flex flex-col items-center lg:flex-row justify-around md:mx-auto py-8 lg:py-10 px-4 lg:px-8 w-full"
-    >
-      <a
-        href="https://github.com/brooksztb"
-        target="_blank"
-        title="Github Profile"
-        class="text-white mx-2 my-2 lg:my-0"
-      >
-        <font-awesome :icon="['fab', 'github']" size="lg" />
-      </a>
-      <a
-        href="https://dev.to/zacann0n"
-        target="_blank"
-        title="Dev.to Profile"
-        class="text-white mx-2 my-2 lg:my-0"
-      >
-        <font-awesome :icon="['fab', 'dev']" size="lg" />
-      </a>
-      <a
-        href="https://twitter.com/zacann0n"
-        target="_blank"
-        title="Twitter Profile"
-        class="text-white mx-2 my-2 lg:my-0"
-      >
-        <font-awesome :icon="['fab', 'twitter']" size="lg" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/zachary-brooks-3b8b5b84/"
-        target="_blank"
-        title="Linkedin Profile"
-        class="text-white mx-2 my-2 lg:my-0"
-      >
-        <font-awesome :icon="['fab', 'linkedin']" size="lg" />
-      </a>
-    </div>
-  </header>
-  <body
-    class="flex flex-1 items-center pl-12 pr-2 lg:pl-px320 xl:pl-px340 lg:pr-5 py-5 h-full min-h-screen mx-auto bg-content-bg text-white"
-  >
-    <slot></slot>
-  </body>
-</div>
+			<transition>
+				<div v-show="mobileNavOpen" class="w-full block flex-grow pt-3">
+					<div class="text-sm flex-grow">
+						<g-link
+							class="inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+							to="/about"
+							active-class="border border-white"
+						>
+							<font-awesome class="mr-2" :icon="['fas', 'user']" size="lg" />
+							<span class="block">About</span>
+						</g-link>
+						<g-link
+							class="inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+							to="/blog"
+							active-class="border border-white"
+						>
+							<font-awesome
+								class="mr-2"
+								:icon="['fas', 'newspaper']"
+								size="lg"
+							/>
+							<span class="block">Blog</span>
+						</g-link>
+						<g-link
+							class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+							to="/skills"
+							active-class="border border-white"
+						>
+							<font-awesome class="mr-2" :icon="['fas', 'code']" size="lg" />
+							<span class="block">Skillset</span>
+						</g-link>
+						<g-link
+							class="inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+							to="/work"
+							active-class="border border-white"
+						>
+							<font-awesome
+								class="mr-2"
+								:icon="['fas', 'project-diagram']"
+								size="lg"
+							/>
+							<span class="block">Work</span>
+						</g-link>
+					</div>
+				</div>
+			</transition>
+		</header>
+		<header
+			class="fixed top-0 xs:hidden md:flex flex-col items-center justify-between h-screen nav-bar md:w-15 lg:w-px300 xl:w-px320"
+		>
+			<div
+				class="flex flex-col items-center justify-start mx-auto w-full pt-0 md:pt-9 px-0 lg:px-8 md:h-full"
+			>
+				<g-link to="/">
+					<g-image
+						class="flex md:inline-flex w-full h-6 sm:h-8 md:h-16 lg:h-logo p-2 lg:pb-5"
+						src="/uploads/logo.svg"
+						alt="zachary brooks logo"
+					/>
+				</g-link>
+
+				<g-link
+					class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+					to="/about"
+					active-class="border border-white"
+				>
+					<font-awesome class="lg:mr-2" :icon="['fas', 'user']" size="lg" />
+					<span class="hidden lg:block">About</span>
+				</g-link>
+				<g-link
+					class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+					to="/blog"
+					active-class="border border-white"
+				>
+					<font-awesome
+						class="lg:mr-2"
+						:icon="['fas', 'newspaper']"
+						size="lg"
+					/>
+					<span class="hidden lg:block">Blog</span>
+				</g-link>
+				<g-link
+					class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+					to="/skills"
+					active-class="border border-white"
+				>
+					<font-awesome class="lg:mr-2" :icon="['fas', 'code']" size="lg" />
+					<span class="hidden lg:block">Skillset</span>
+				</g-link>
+				<g-link
+					class="flex md:inline-flex items-center justify-center hover:bg-white hover:text-logo-gray text-white font-bold my-1 py-2 lg:px-6 rounded w-full"
+					to="/work"
+					active-class="border border-white"
+				>
+					<font-awesome
+						class="lg:mr-2"
+						:icon="['fas', 'project-diagram']"
+						size="lg"
+					/>
+					<span class="hidden lg:block">Work</span>
+				</g-link>
+			</div>
+			<div
+				class="flex flex-col items-center lg:flex-row justify-around md:mx-auto py-8 lg:py-10 px-4 lg:px-8 w-full"
+			>
+				<a
+					href="https://github.com/brooksztb"
+					target="_blank"
+					title="Github Profile"
+					class="text-white mx-2 my-2 lg:my-0"
+				>
+					<font-awesome :icon="['fab', 'github']" size="lg" />
+				</a>
+				<a
+					href="https://dev.to/zacann0n"
+					target="_blank"
+					title="Dev.to Profile"
+					class="text-white mx-2 my-2 lg:my-0"
+				>
+					<font-awesome :icon="['fab', 'dev']" size="lg" />
+				</a>
+				<a
+					href="https://twitter.com/zacann0n"
+					target="_blank"
+					title="Twitter Profile"
+					class="text-white mx-2 my-2 lg:my-0"
+				>
+					<font-awesome :icon="['fab', 'twitter']" size="lg" />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/zachary-brooks-3b8b5b84/"
+					target="_blank"
+					title="Linkedin Profile"
+					class="text-white mx-2 my-2 lg:my-0"
+				>
+					<font-awesome :icon="['fab', 'linkedin']" size="lg" />
+				</a>
+			</div>
+		</header>
+		<body
+			class="flex flex-1 items-center pl-3 pr-3 pt-px64 md:pt-5 lg:pl-px320 xl:pl-px340 lg:pr-5 py-5 h-full min-h-screen mx-auto bg-content-bg text-white"
+		>
+			<slot></slot>
+		</body>
+	</div>
 </template>
 
 <static-query>
@@ -102,15 +185,20 @@ query {
 
 <script>
 export default {
-  name: "App"
+	name: 'App',
+	data() {
+		return {
+			mobileNavOpen: false,
+		};
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 .nav-bar {
-  background: -webkit-linear-gradient(121deg, #292f35 50%, #84161b 50%);
-  background: -o-linear-gradient(121deg, #292f35 50%, #84161b 50%);
-  background: -moz-linear-gradient(121deg, #292f35 50%, #84161b 50%);
-  background: linear-gradient(121deg, #292f35 50%, #84161b 50%);
+	background: -webkit-linear-gradient(121deg, #292f35 50%, #84161b 50%);
+	background: -o-linear-gradient(121deg, #292f35 50%, #84161b 50%);
+	background: -moz-linear-gradient(121deg, #292f35 50%, #84161b 50%);
+	background: linear-gradient(121deg, #292f35 50%, #84161b 50%);
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,56 @@
 module.exports = {
-  theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px"
-    },
-    flex: {
-      "1": "1 0 auto"
-    },
-    fontFamily: {
-      display: ["Oswald", "sans-serif"],
-      body: ["Oswald", "sans-serif"]
-    },
-    extend: {
-      colors: {
-        "logo-red": "#84161b",
-        "logo-gray": "#292f35",
-        "content-bg": "#222831"
-      },
-      spacing: {
-        px10: "10px",
-        px300: "300px",
-        px320: "320px",
-        px340: "340px",
-        px900: "900px"
-      },
-      opacity: {
-        inherit: "inherit",
-        "80": ".80"
-      },
-      height: {
-        logo: "220px",
-        "screen-1/3": "33vh"
-      },
-      width: {
-        auto: "auto"
-      },
-      minHeight: {
-        "38": "38vh",
-        px250: "250px"
-      },
-      minWidth: {
-        "70": "70%"
-      }
-    }
-  },
-  variants: {
-    backgroundColor: ["responsive", "hover", "focus", "active"],
-    transitionProperty: ["responsive", "hover", "focus"]
-  },
-  plugins: []
+	theme: {
+		screens: {
+			xs: '375px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		flex: {
+			'1': '1 0 auto',
+		},
+		fontFamily: {
+			display: ['Oswald', 'sans-serif'],
+			body: ['Oswald', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'logo-red': '#84161b',
+				'logo-gray': '#292f35',
+				'content-bg': '#222831',
+			},
+			spacing: {
+				px10: '10px',
+				px64: '64px',
+				px300: '300px',
+				px320: '320px',
+				px340: '340px',
+				px900: '900px',
+			},
+			opacity: {
+				inherit: 'inherit',
+				'80': '.80',
+			},
+			height: {
+				logo: '220px',
+				'screen-1/3': '33vh',
+			},
+			width: {
+				auto: 'auto',
+			},
+			minHeight: {
+				'38': '38vh',
+				px250: '250px',
+			},
+			minWidth: {
+				'70': '70%',
+			},
+		},
+	},
+	variants: {
+		backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+		transitionProperty: ['responsive', 'hover', 'focus'],
+	},
+	plugins: [],
 };
