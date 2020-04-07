5c949301de2e204005b0c13c9f10bff1f85df938
Brooksztb <brooksztb@gmail.com>
2020-04-07 16:41:56 -0600

    Added Datocms plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,49 +7,36 @@ const tailwindcss = require('tailwindcss');
 const config = require('./src/content/data/config.json');
 
 module.exports = {
-	siteName: config.title,
-	metadata: {
-		siteName: config.title,
-		siteDescription: config.description,
-		cloudinaryUrl: config.cloudinary_url,
-	},
-	siteUrl: 'https://zacharybrooks.io',
-	titleTemplate: `%s - ${config.title}`,
-	icon: '/src/static/favicon.ico',
-	transformers: {
-		remark: {
-			externalLinksTarget: '_blank',
-			externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-		},
-	},
-	css: {
-		loaderOptions: {
-			postcss: {
-				plugins: [tailwindcss],
-			},
-		},
-	},
-	plugins: [
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				typeName: 'Blog',
-				baseDir: './src/content/',
-				path: 'blog/**/*.md',
-				resolveAbsolutePaths: true,
-				remark: {
-					externalLinksTarget: '_blank',
-					externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-				},
-			},
-		},
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				typeName: 'Content',
-				baseDir: './src/content/pages/',
-				path: '*.md',
-			},
-		},
-	],
+  siteName: config.title,
+  metadata: {
+    siteName: config.title,
+    siteDescription: config.description,
+  },
+  siteUrl: 'https://zacharybrooks.io',
+  titleTemplate: `%s - ${config.title}`,
+  icon: '/src/static/favicon.ico',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+    },
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [tailwindcss],
+      },
+    },
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-datocms',
+      options: {
+        apiToken: '318db82393a7a79ca8556f7aff0238', // required move to a env file
+        previewMode: true,
+        apiUrl: 'https://graphql.datocms.com/preview',
+        typeName: 'DatoCms',
+      },
+    },
+  ],
 };
